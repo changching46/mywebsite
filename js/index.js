6b0897c19d7b20b55a681ba60763d5352ad20bfb
changching46 <chingchang0406@gmail.com>
2024-11-10 22:03:38 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,131 @@
+ 
+  let slideIndex1 = 1;
+  let slideIndex2 = 1;
+  let slideIndex3 = 1;
+  let slideIndex4 = 1;
+  let slideIndex5 = 1;
+  showSlides1(slideIndex1);
+  showSlides2(slideIndex2);
+  showSlides3(slideIndex3);
+  showSlides4(slideIndex4);
+  showSlides5(slideIndex5);
+
+  function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
+  }
+  
+
+  function currentSlide1(n) {
+    showSlides1(slideIndex1 = n);
+  }
+  
+  function showSlides1(n) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlides1");
+    let dots = document.getElementsByClassName("dot1");
+    if (n > slides.length) {slideIndex1 = 1}
+    if (n < 1) {slideIndex1 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex1-1].style.display = "block";
+    dots[slideIndex1-1].className += " active";
+  }
+  
+  function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+  }
+  
+  function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+  }
+  
+  function showSlides2(n) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlides2");
+    let dots = document.getElementsByClassName("dot2"); 
+    if (n > slides.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex2-1].style.display = "block";
+    dots[slideIndex2-1].className += " active";
+  }
+  
+  function plusSlides3(n) {
+    showSlides3(slideIndex3 += n);
+  }
+  
+
+  function currentSlide3(n) {
+    showSlides3(slideIndex3 = n);
+  }
+  
+  function showSlides3(n) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlides3");
+    let dots = document.getElementsByClassName("dot3"); 
+    if (n > slides.length) {slideIndex3 = 1}
+    if (n < 1) {slideIndex3 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex3-1].style.display = "block";
+    dots[slideIndex3-1].className += " active";
+  }
+  
+  function plusSlides4(n) {
+    showSlides4(slideIndex4 += n);
+  }
+  function currentSlide4(n) {
+    showSlides4(slideIndex4 = n);
+  }
+  
+  function showSlides4(n) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlides4");
+    let dots = document.getElementsByClassName("dot4"); 
+    if (n > slides.length) {slideIndex4 = 1}
+    if (n < 1) {slideIndex4 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex4-1].style.display = "block";
+    dots[slideIndex4-1].className += " active";
+  }
+  
+  function plusSlides5(n) {
+    showSlides5(slideIndex5 += n);
+  }
+  function currentSlide5(n) {
+    showSlides5(slideIndex5 = n);
+  }
+  
+  function showSlides5(n) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlides5");
+    let dots = document.getElementsByClassName("dot5"); 
+    if (n > slides.length) {slideIndex5 = 1}
+    if (n < 1) {slideIndex5 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex5-1].style.display = "block";
+    dots[slideIndex5-1].className += " active";
+  }
